a3f35c6681d1faf74d38973fb6103d38746df9f1
Oliver Rose <hello@olivermrose.com>
2025-11-11 03:32:06 -0500

    refactor(perf): prevent unnecessary rejoins
    not useful right now but will be in the future once i implement more routes

--- src/routes/(main)/channels/[username]/+page.ts
+++ src/routes/(main)/channels/[username]/+page.ts
@@ -5,12 +5,17 @@ import { app } from "$lib/app.svelte";
 export async function load({ params, parent }) {
 	if (dev) await parent();
 
-	await app.joined?.leave();
-
 	const channel = app.channels.find((c) => c.user.username === params.username);
-	if (!channel) error(404);
 
-	await channel.join();
+	if (!channel) {
+		await app.joined?.leave();
+		error(404);
+	}
+
+	if (app.joined !== channel) {
+		await app.joined?.leave();
+		await channel.join();
+	}
 
 	return { channel };
 }
