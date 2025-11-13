287d5137de90c12daf61108b226650288b2f7c7d
Oliver Rose <hello@olivermrose.com>
2025-11-13 04:14:23 -0500

    use parent

--- src/routes/(main)/whispers/[id]/+page.ts
+++ src/routes/(main)/whispers/[id]/+page.ts
@@ -1,9 +1,9 @@
 import { error } from "@sveltejs/kit";
-import { page } from "$app/state";
-import type { Whisper } from "$lib/models";
 
-export function load({ params }) {
-	const whisper: Whisper | undefined = page.data.whispers.get(params.id);
+export async function load({ parent, params }) {
+	const { whispers } = await parent();
+	const whisper = whispers.get(params.id);
+
 	if (!whisper) error(404);
 
 	whisper.unread = 0;
