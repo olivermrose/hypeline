fc30a025f016a20f0028ed5a69fb11231b814079
Oliver Rose <hello@olivermrose.com>
2025-11-18 01:21:24 -0500

    feat(commands): add `founders` command

---
+++ src/lib/commands/built-in/founders.ts
@@ -0,0 +1,40 @@
+import { twitchGql as gql } from "$lib/graphql/function";
+import { SystemMessage } from "$lib/models/message/system-message";
+import { defineCommand } from "../";
+
+export default defineCommand({
+	provider: "Built-in",
+	name: "founders",
+	description: "Display a list of founders for this channel",
+	async exec(_, channel) {
+		const message = new SystemMessage();
+
+		const { user } = await channel.client.send(
+			gql(`query GetFounders($id: ID!) {
+				user(id: $id) {
+					channel {
+						founders {
+							user {
+								displayName
+							}
+						}
+					}
+				}
+			}`),
+			{ id: channel.id },
+		);
+
+		const founders =
+			user?.channel?.founders
+				?.flatMap((founder) => (founder?.user ? [founder.user.displayName] : []))
+				.sort() ?? [];
+
+		if (!founders.length) {
+			message.text = "This channel has no founders.";
+		} else {
+			message.text = `Channel founders (${founders.length}): ${founders.join(", ")}`;
+		}
+
+		channel.chat.addMessage(message);
+	},
+});
--- src/lib/commands/built-in/index.ts
+++ src/lib/commands/built-in/index.ts
@@ -1,5 +1,6 @@
+import founders from "./founders";
 import join from "./join";
 import leave from "./leave";
 import refreshEmotes from "./refresh-emotes";
 
-export const commands = [join, leave, refreshEmotes];
+export const commands = [founders, join, leave, refreshEmotes];
