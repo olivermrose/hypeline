c3b1d43182b8156f16c69ff96cf4948683a9cf56
Oliver Rose <hello@olivermrose.com>
2025-11-22 06:46:27 -0500

    wip

--- src/lib/components/EmotePicker.svelte
+++ src/lib/components/EmotePicker.svelte
@@ -1,21 +1,14 @@
 <script lang="ts">
-	import { invoke } from "@tauri-apps/api/core";
 	import { Popover, Tabs } from "bits-ui";
 	import { onMount } from "svelte";
 	import { VList } from "virtua/svelte";
 	import { app } from "$lib/app.svelte";
-	import type { Emote, EmoteProvider } from "$lib/emotes";
+	import type { Emote, EmoteProvider, EmoteSet } from "$lib/emotes";
 	import type { Channel } from "$lib/models/channel.svelte";
 	import type { User } from "$lib/models/user.svelte";
-	import type { UserEmote } from "$lib/twitch/api";
+	import Input from "./ui/Input.svelte";
 
 	type EmoteSetOwner = Pick<User, "id" | "username" | "displayName" | "avatarUrl">;
-
-	interface EmoteSet {
-		owner: EmoteSetOwner;
-		global: boolean;
-		emotes: Emote[];
-	}
 
 	interface Props {
 		channel: Channel;
@@ -47,67 +40,39 @@
 		},
 	};
 
-	const emoteSets = new Map<string, EmoteSet>();
-
 	let sorted = $state.raw<EmoteSet[]>([]);
 
 	onMount(async () => {
-		const providerGlobals = Map.groupBy(app.emotes.values(), (emote) => emote.provider);
+		if (!app.user) return;
 
-		for (const [provider, emotes] of providerGlobals) {
-			// This is never true but TypeScript needs to believe it to narrow
-			if (provider === "Twitch") continue;
+		const channelSet = app.user.emoteSets.find((set) => set.owner.id === channel.id);
 
-			emoteSets.set(provider, {
-				owner: mockAccounts[provider],
-				global: true,
-				emotes,
-			});
+		if (channelSet) {
+			channelSet.emotes.push(...channel.emotes.values().filter((e) => e.provider === "7TV"));
 		}
 
-		const emotes = await invoke<UserEmote[]>("get_user_emotes");
-		const grouped = Map.groupBy(emotes, (emote) => emote.owner_id || "twitch");
+		// const providerGlobals = Map.groupBy(app.emotes.values(), (emote) => emote.provider);
 
-		for (const [id, emotes] of grouped) {
-			const owner = await app.twitch.users.fetch(id, {
-				by: id === "twitch" ? "login" : "id",
-			});
+		// for (const [provider, emotes] of providerGlobals) {
+		// 	// This is never true but TypeScript needs to believe it to narrow
+		// 	if (provider === "Twitch") continue;
 
-			const mapped = emotes.map<Emote>((emote) => ({
-				provider: "Twitch",
-				id: emote.id,
-				name: emote.name,
-				width: 56,
-				height: 56,
-				srcset: emote.scale.map(
-					(d) =>
-						`https://static-cdn.jtvnw.net/emoticons/v2/${emote.id}/default/dark/${d} ${d}x`,
-				),
-			}));
+		// 	emoteSets.set(provider, {
+		// 		owner: mockAccounts[provider],
+		// 		global: true,
+		// 		emotes,
+		// 	});
+		// }
 
-			if (owner.id === channel.id) {
-				mapped.push(...channel.emotes.values().filter((e) => e.provider === "7TV"));
-			}
+		sorted = app.user.emoteSets.toSorted((a, b) => {
+			if (a.owner.id === channel.id) return -1;
+			if (b.owner.id === channel.id) return 1;
 
-			emoteSets.set(id, {
-				owner,
-				global: id === "twitch",
-				emotes: mapped,
-			});
-		}
+			if (a.global && !b.global) return 1;
+			if (!a.global && b.global) return -1;
 
-		sorted = emoteSets
-			.values()
-			.toArray()
-			.sort((a, b) => {
-				if (a.owner.id === channel.id) return -1;
-				if (b.owner.id === channel.id) return 1;
-
-				if (a.global && !b.global) return 1;
-				if (!a.global && b.global) return -1;
-
-				return a.owner.username.localeCompare(b.owner.username);
-			});
+			return a.owner.username.localeCompare(b.owner.username);
+		});
 	});
 
 	function appendEmote(name: string) {
@@ -141,13 +106,13 @@
 
 	<Popover.Portal>
 		<Popover.Content
-			class="overflow-hidden rounded-md border"
+			class="bg-sidebar overflow-hidden rounded-md border"
 			side="top"
 			sideOffset={12}
 			collisionPadding={8}
 		>
 			<Tabs.Root class="flex max-h-100" orientation="vertical">
-				<Tabs.List class="bg-sidebar flex flex-col gap-3 overflow-y-auto p-2">
+				<Tabs.List class="flex flex-col gap-3 overflow-y-auto p-2">
 					{#each sorted as set (set.owner.id)}
 						<Tabs.Trigger class="group" value={set.owner.displayName}>
 							<img
@@ -163,10 +128,20 @@
 
 				{#each sorted as set (set.owner.id)}
 					<Tabs.Content
-						class="bg-muted w-[calc(var(--spacing)*56+64px)] border-l p-2"
+						class="flex w-[calc(var(--spacing)*56+64px)] flex-col border-l"
 						value={set.owner.displayName}
 					>
-						<VList id="emote-vlist" class="overflow-y-auto" data={chunk(set.emotes)}>
+						<Input
+							class="focus-visible:outline-input rounded-none rounded-tr-md border-none focus-visible:ring-0"
+							type="search"
+							placeholder="Search {set.name}..."
+						/>
+
+						<VList
+							id="emote-vlist"
+							class="grow overflow-y-auto border-t p-2"
+							data={chunk(set.emotes)}
+						>
 							{#snippet children(emotes)}
 								<div class="emote-row mb-2 flex items-center gap-2">
 									{#each emotes as emote (emote.id)}
