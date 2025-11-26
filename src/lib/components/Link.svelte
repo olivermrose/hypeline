5cb7e9b9e2ce9a689ffdaa358a63f9e7b86df778
Oliver Rose <hello@olivermrose.com>
2025-11-26 09:17:38 -0500

    a11y

--- src/lib/components/Link.svelte
+++ src/lib/components/Link.svelte
@@ -1,20 +1,23 @@
 <script lang="ts">
 	import { openUrl } from "@tauri-apps/plugin-opener";
-	import type { HTMLAnchorAttributes } from "svelte/elements";
+	import type { HTMLAttributes } from "svelte/elements";
 
-	interface Props extends HTMLAnchorAttributes {
+	interface Props extends HTMLAttributes<HTMLSpanElement> {
 		href: string;
 	}
 
 	const { children, href, ...rest }: Props = $props();
 </script>
 
-<!-- svelte-ignore a11y_click_events_have_key_events -->
 <span
 	class="text-twitch-link underline hover:cursor-pointer"
 	role="link"
-	tabindex="-1"
+	tabindex="0"
 	onclick={() => openUrl(href)}
+	onkeydown={(event) => {
+		if (event.key === "Enter") openUrl(href);
+	}}
+	{...rest}
 >
 	{@render children?.()}
 </span>
