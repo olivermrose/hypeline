198c7b783e2649159e1193ece784dca958c0f935
Oliver Rose <hello@olivermrose.com>
2025-11-28 04:28:17 -0500

    quick actions

--- src/lib/components/QuickActions.svelte
+++ src/lib/components/QuickActions.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
-	import { Toolbar } from "bits-ui";
+	import type { Component, ComponentProps } from "svelte";
 	import ArrowBendUpLeft from "~icons/ph/arrow-bend-up-left";
 	import Clipboard from "~icons/ph/clipboard";
 	import Clock from "~icons/ph/clock";
 	import Gavel from "~icons/ph/gavel";
 	import Trash from "~icons/ph/trash";
 	import type { UserMessage } from "$lib/models/message/user-message";
-	import { cn } from "$lib/util";
-	import { Separator } from "./ui/separator";
+	import { Button } from "./ui/button";
+	import * as ButtonGroup from "./ui/button-group";
+	import * as Tooltip from "./ui/tooltip";
 
 	interface Props {
 		class?: string;
@@ -15,57 +16,69 @@
 	}
 
 	const { class: className, message }: Props = $props();
-
-	async function copy() {
-		await navigator.clipboard.writeText(message.text);
-	}
 </script>
 
-<Toolbar.Root class={cn("bg-muted flex items-center gap-x-1 rounded-sm border p-0.5", className)}>
-	<Toolbar.Button
-		class="hover:bg-muted-foreground/50 flex items-center justify-center rounded-sm p-1"
-		title="Copy message"
-		onclick={copy}
-	>
-		<Clipboard class="size-4" />
-	</Toolbar.Button>
+<ButtonGroup.Root class={className}>
+	{@render button({
+		tooltip: "Copy message",
+		icon: Clipboard,
+		onclick: async () => await navigator.clipboard.writeText(message.text),
+	})}
 
-	<Toolbar.Button
-		class="hover:bg-muted-foreground/50 flex items-center justify-center rounded-sm p-1"
-		title="Reply to {message.author.displayName}"
-		onclick={() => {
+	{@render button({
+		tooltip: `Reply to ${message.author.displayName}`,
+		icon: ArrowBendUpLeft,
+		onclick: () => {
 			message.channel.chat.replyTarget = message;
 			message.channel.chat.input?.focus();
-		}}
-	>
-		<ArrowBendUpLeft class="size-4" />
-	</Toolbar.Button>
+		},
+	})}
 
 	{#if message.actionable}
-		<Separator orientation="vertical" />
+		<ButtonGroup.Separator orientation="vertical" />
 
-		<Toolbar.Button
-			class="hover:bg-muted-foreground/50 flex items-center justify-center rounded-sm p-1 text-blue-400"
-			title="Delete message"
-			onclick={() => message.delete()}
-		>
-			<Trash class="size-4" />
-		</Toolbar.Button>
+		{@render button({
+			class: "text-red-400",
+			tooltip: "Delete message",
+			icon: Trash,
+			onclick: async () => await message.delete(),
+		})}
 
-		<Toolbar.Button
-			class="hover:bg-muted-foreground/50 flex items-center justify-center rounded-sm p-1 text-yellow-400"
-			title="Timeout {message.author.displayName} for 10 minutes"
-			onclick={() => message.viewer?.timeout({ duration: 600 })}
-		>
-			<Clock class="size-4" />
-		</Toolbar.Button>
+		{@render button({
+			class: "text-red-400",
+			tooltip: `Timeout ${message.author.displayName} for 10 minutes`,
+			icon: Clock,
+			onclick: async () => await message.viewer?.timeout({ duration: 600 }),
+		})}
 
-		<Toolbar.Button
-			class="hover:bg-muted-foreground/50 text-destructive flex items-center justify-center rounded-sm p-1"
-			title="Ban {message.author.displayName}"
-			onclick={() => message.viewer?.ban()}
-		>
-			<Gavel class="size-4" />
-		</Toolbar.Button>
+		{@render button({
+			class: "text-red-400",
+			tooltip: `Ban ${message.author.displayName}`,
+			icon: Gavel,
+			onclick: async () => await message.viewer?.ban(),
+		})}
 	{/if}
-</Toolbar.Root>
+</ButtonGroup.Root>
+
+{#snippet button(props: ComponentProps<typeof Button> & { icon: Component; tooltip: string })}
+	<Tooltip.Root>
+		<Tooltip.Trigger>
+			{#snippet child({ props: childProps })}
+				<Button
+					size="icon-sm"
+					variant="secondary"
+					aria-label={props.tooltip}
+					onclick={() => message.viewer?.ban()}
+					{...props}
+					{...childProps}
+				>
+					<props.icon />
+				</Button>
+			{/snippet}
+		</Tooltip.Trigger>
+
+		<Tooltip.Content side="top">
+			{props.tooltip}
+		</Tooltip.Content>
+	</Tooltip.Root>
+{/snippet}
